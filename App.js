4c4de7b280ce1f5853082b200778c9578bb94908
Aluno <aluno@s4mac2.local>
2018-05-23 20:02:38 +0100

    native app

--- App.js
+++ App.js
@@ -1,20 +1,40 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, ScrollView } from 'react-native';
+
+const Agradecimento = ({pessoa}) => {
+  return <View>
+    <Text>Olá! {pessoa}</Text>
+    </View>
+}
+
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
+      <ScrollView>
+         <View> 
+          <Text>Open up App.js to start working on your app!</Text>
+          <Text>Changes you make will automatically reload.</Text>
+          <Text>Shake your phone to open the developer menu.</Text>
+          <Text>Shake your phone to open the developer menu.</Text>
+          <Text>Shake your phone to open the developer menu.</Text>
+          <Image source={{uri:'http://www.arcadenoe.pt/img/race/big_978.jpg'}}
+                style={{width: 221, height: 228}}/> 
+          <Agradecimento pessoa="quack"/>
+          <Agradecimento pessoa="woof woof"/>
+          <Agradecimento pessoa="meow meow"/>
+          <Agradecimento pessoa="kwiwi"/>
+        </View>
+      </ScrollView>
     );
   }
 }
 
 
 const styles = StyleSheet.create({
+  textStyle:{
+    fontSize:16,
+  },
   container: {
     flex: 1,
     backgroundColor: '#fff',
